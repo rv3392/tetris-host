ab2b18b45d69ede3320f021772ce553a441d2037
rv3392 <richalverma00@gmail.com>
2020-06-26 15:58:34 +1000

    Created libgdx project

---
+++ desktop/src/tech/richal/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.richal.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.richal.tetris.Tetris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tetris(), config);
+	}
+}
